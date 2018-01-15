95e531df374fcc326970c420eaed3a5967d6d2ab
zinoki <zinouye@gmail.com>
2018-01-15 13:19:24 -0800

    Implemented Stringify JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,30 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+
+  } else if (Array.isArray(obj)) {
+    var result = '';
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ',';
+    }
+    return '[' + result.slice(0, result.length-1) + ']';
+  
+  } else if (typeof obj === 'object'){
+    var result = '';
+    for (var key in obj) {
+      if (key === 'functions' || key === 'undefined') {
+        continue;
+      }
+      result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    return '{' + result.slice(0, result.length-1) + '}';
+
+  } 
+  
 };

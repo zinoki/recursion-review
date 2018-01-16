1ec0e989c258c4a2b8d5715adfadb036733a6375
zinoki <zinouye@gmail.com>
2018-01-15 16:20:13 -0800

    Finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // Iterate through DOM Nodes & Children
+  // Look for targetClassName, add it to results array
+  // Recursively call func. for each child node with above steps
+
+  results = [];
+
+  var getElementsByClassNameRecursive = function(className, node) {
+    if (node.className !== undefined && node.className.split(" ").indexOf(className) !== -1) {
+      results.push(node);
+    }
+
+    for (var i=0; i < node.childNodes.length; i++){
+      getElementsByClassNameRecursive(className,node.childNodes[i]);
+    }
+    
+    return;
+
+  };
+
+  getElementsByClassNameRecursive(className, document.body);
+
+  return results;
+
 };
